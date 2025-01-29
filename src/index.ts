76b38508576127e3b27b125b3bd6d0cbd00ca011
Abdulfettah <fettahaud@gmail.com>
2025-01-29 22:57:34 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { accordion } from '$utils/accordion';
 import { experiences } from '$utils/experiences';
-import { stickyHero } from '$utils/stickyHero';
-
 import { gsapSmoothScroll } from '$utils/gsapSmoothScroll';
 import { homeMain } from '$utils/homeMain';
 import { linesAnimation } from '$utils/linesAnimation';
+import { stickyHero } from '$utils/stickyHero';
 import { swiperSliders } from '$utils/swiperSliders';
 import { textMask } from '$utils/textMask';
 window.Webflow = window.Webflow || [];
