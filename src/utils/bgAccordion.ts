e64e2de970a0bcf07e38791e473314c5795482f6
Mico Irvin <104616752+micoirvin@users.noreply.github.com>
2025-02-09 18:46:22 +0800

    Accordion close on click. Room indivi animation

--- src/utils/bgAccordion.ts
+++ src/utils/bgAccordion.ts
@@ -1,40 +1,40 @@
 export const bgAccordion = () => {
-  // `.bg-accordion_item-radio-field.fs-cmsfilter_active~.bg-accordion_names-article .bg-accordion_item-sizer`;
   const section = document.querySelector('.section_bg-accordion');
   if (!section) return;
   const sizers = section.querySelectorAll('.bg-accordion_item-sizer');
-  section.addEventListener('click', (e) => {
-    const item = e.target.closest('.bg-accordion_names-item');
-    if (!item) return;
-    const label = item.querySelector('.bg-accordion_item-radio-field');
-    if (!label) return;
-    const sizer = item.querySelector('.bg-accordion_item-sizer');
-    if (!sizer) return;
 
-    const closable = section.getAttribute('accordion-closable');
-    const x = e.target.closest('.bg-accordion_item-x');
-    // if (closable && window.innerWidth <= 767 && x) {
-    //   // const radio = item.querySelector('input[type=radio]');
-    //   // radio?.removeAttribute('checked');
-    //   // const closer = section.querySelector('input[type=radio].bg-accordion_item-closer');
-    //   // console.log(closer);
-    //   // closer?.setAttribute('checked', 'checked');
-    //   // console.log('hello');
-    // }
+  const config = {
+    attributes: true,
+    subtree: true,
+    attributeFilter: ['class'],
+  };
 
-    if (x) {
-      console.log('x');
-      sizers.forEach((s) => {
-        s.style.height = '0px';
-      });
-    } else if (!label.classList.contains('fs-cmsfilter_active')) {
-      sizers.forEach((s) => {
-        s.style.height = '0px';
-      });
-      const paragraph = item.querySelector('.bg-accordion_item-paragraph');
-      if (!paragraph) return;
-      const height = paragraph.getBoundingClientRect().height;
-      sizer.style.height = String(height) + 'px';
+  const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+        const target = mutation.target;
+        if (!target.classList.contains('bg-accordion_item-radio-field')) return;
+
+        sizers.forEach((s) => {
+          s.style.height = '0px';
+        });
+
+        let active = section.querySelector('.bg-accordion_item-radio-field.fs-cmsfilter_active');
+        if (active) {
+          console.log(active);
+          const parent = active.parentElement;
+          if (!parent) return;
+          const paragraph = parent.querySelector('.bg-accordion_item-paragraph');
+          if (!paragraph) return;
+          const height = paragraph.getBoundingClientRect().height;
+          const sizer = parent.querySelector('.bg-accordion_item-sizer');
+          if (!sizer) return;
+          sizer.style.height = String(height) + 'px';
+        }
+      }
     }
-  });
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(section, config);
 };
