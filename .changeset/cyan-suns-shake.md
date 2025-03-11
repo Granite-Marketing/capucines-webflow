9dd7f24d5f44c75af55fc0e03683ad8fb1c92e9f
Stephen Anindo <sanindo@hotmail.co.uk>
2025-03-11 16:58:09 +0000

    area and scroll button updates

---
+++ .changeset/cyan-suns-shake.md
@@ -0,0 +1,5 @@
+---
+'@granite-marketing/editoria': patch
+---
+
+area updates and scroll button fixes
--- src/utils/bookingButtonOnScoll.ts
+++ src/utils/bookingButtonOnScoll.ts
@@ -5,20 +5,34 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const bookingButtonOnScroll = () => {
     const bookingButton = document.querySelector("#floating-button");
+    const footerElement = document.querySelector(".section_cta");
     
-    if (!bookingButton) return;
+    if (!bookingButton || !footerElement) return;
+
+    let mm = gsap.matchMedia();
 
     // Set initial opacity
     gsap.set(bookingButton, { opacity: 0, visibility: "hidden" });    
 
-    // Create the scroll-triggered animation
-    gsap.to(bookingButton, {
-        opacity: 1,
-        visibility: "visible",
-        scrollTrigger: {
-            trigger: ".page-wrapper",
-            start: "top top-=200",
-            toggleActions: "play none none reverse",
-        }
-    });
+    mm.add(
+        {
+          isMobile: 'screen and (max-width: 767px)',
+          isDesktop: 'screen and (min-width: 768px)',
+        },
+        (context) => {
+          const { conditions } = context;
+
+          // Create the scroll-triggered animation
+          gsap.to(bookingButton, {
+            opacity: 1,
+            visibility: "visible",
+            scrollTrigger: {
+                trigger: ".page-wrapper",
+                endTrigger: ".section_cta",
+                start: "top top-=200",
+                end: "top bottom",
+                toggleActions: "play reverse play reverse",
+            }
+          });
+        })    
 }
